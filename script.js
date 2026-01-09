a065294194cac7aa49962c7f860b8a6d5ae110fb
Apurv1512 <apurvwaghmare204@gmail.com>
2026-01-09 22:52:04 +0530

    Initial commit - Task 01 Responsive Landing Page

---
+++ script.js
@@ -0,0 +1,11 @@
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (!navbar) return;
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
